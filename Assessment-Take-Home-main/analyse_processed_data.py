0b60d9c8a53f08c428922df1a91c792880d6323e
howardman <howardppg@gmail.com>
2024-07-17 19:01:37 +0100

    Finished Task 2

--- Assessment-Take-Home-main/analyse_processed_data.py
+++ Assessment-Take-Home-main/analyse_processed_data.py
@@ -6,16 +6,10 @@ import altair as alt
 
 def extract_decade_from_year(year: int) -> int:
     """Given a year, returns its decade."""
-    pass
 
+    result = (year // 10) * 10
 
-def create_decades_list(year_input: pd.Series) -> list[int]:
-    """
-    Given a pd.Series object containing years, return a list, where
-    elements are distinct, and each element is a year corresponding to the decade
-    of every year in the input
-    """
-    pass
+    return result
 
 
 def create_decade_releases_png(year_input: pd.Series) -> None:
@@ -24,7 +18,19 @@ def create_decade_releases_png(year_input: pd.Series) -> None:
     format, where each colour corresponds to a decade, and the angle of each
     sector corresponds to the number of books released in that decade.
     """
-    pass
+    
+    decades_series = year_input.apply(extract_decade_from_year)
+
+    data = decades_series.value_counts().reset_index()
+
+    data = data.rename(columns={'year': 'Decade'})
+
+    chart = alt.Chart(data, title="Book Releases by Decade").mark_arc().encode(
+        color='Decade:N',
+        theta='count:Q'
+    )
+
+    chart.save('decade_releases.png')
 
 
 def create_top_authors_png(authors_input: pd.Series) -> None:
@@ -33,10 +39,26 @@ def create_top_authors_png(authors_input: pd.Series) -> None:
     bar chart, where the x-axis has names of the ten most-rated authors, and
     the y-axis has the total number of ratings for each author.
     """
-    pass
+
+    data = authors_input.value_counts().reset_index()
+
+    data = data.rename(columns={'author_name': 'Author'})
+
+    top_ten_data = data.iloc[[str(i) for i in range(10)]]
+
+    chart = alt.Chart(top_ten_data, title='Top Authors Read').mark_bar().encode(
+        x=alt.X('Author:N').sort('-y'),
+        y='count:Q',
+        color='Author:N'
+    )
+
+    chart.save('top_authors.png')
 
 
 if __name__ == "__main__":
     
     all_data = pd.read_csv("PROCESSED_DATA.csv")
 
+    create_decade_releases_png(all_data['year'])
+
+    create_top_authors_png(all_data['author_name'])
